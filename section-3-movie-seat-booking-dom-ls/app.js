e3c2f3f9ff58502a4b91953b0f9180feec35af02
luke-h1 <luke.howsam@yahoo.com>
2020-08-25 16:44:42 +0100

    add project

--- section-3-movie-seat-booking-dom-ls/app.js
+++ section-3-movie-seat-booking-dom-ls/app.js
@@ -3,9 +3,9 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
-let ticketPrice = parseInt(movieSelect.value);
 populateUI();
 
+let ticketPrice = parseInt(movieSelect.value);
 
 // console.log(typeof ticketPrice);
 
@@ -15,36 +15,34 @@ function setMovieData(movieIndex, moviePrice) {
   localStorage.setItem('selectedMoviePrice', moviePrice);
 }
 
+// updaetSelectedCount pseudo
+// COPY SELECTED SEATS INTO ARR
+// MAP THRU ARRAY
+// RETURN NEW ARR OF INDEXES
 
 // UPDATE TOTAL PRICE AND TICKET COUNT
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
-  // COPY SELECTED SEATS INTO ARR
-  // MAP THRU ARRAY
-  // RETURN NEW ARR OF INDEXES
-  const seatsIndex = [...selectedSeats].map((seat) => {
-    [...seats].indexOf(seat);
-  });
-  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex)); // turn arr to str
-  // console.log(seatsIndex)
 }
 
-// GET DATA FROM LS & POPULATE UI 
-function populateUI(){
-  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats')); 
-  if(selectedSeats !== null && selectedSeats.length > 0){
+// GET DATA FROM LS & POPULATE UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
     seats.forEach((seat, index) => {
-      if(selectedSeats.indexOf(index) > -1 ){
+      if (selectedSeats.indexOf(index) > -1) {
         seat.classList.add('selected');
       }
-    })
-  } 
-  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex'); 
-  if(selectedMovieIndex !== null){
-    movieSelect.selectedIndex = selectedMovieIndex; 
+    });
+  }
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
   }
 }
 
@@ -65,3 +63,6 @@ container.addEventListener('click', (e) => {
     updateSelectedCount();
   }
 });
+
+// INIT COUNT & TOTAL ON DOM LOAD
+updateSelectedCount();
