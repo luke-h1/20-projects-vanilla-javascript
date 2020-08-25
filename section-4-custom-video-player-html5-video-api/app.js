4e56f7a161484edf47a053887a1fb46af49c8b08
luke-h1 <luke.howsam@yahoo.com>
2020-08-25 18:13:08 +0100

    update

--- section-4-custom-video-player-html5-video-api/app.js
+++ section-4-custom-video-player-html5-video-api/app.js
@@ -4,35 +4,52 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
-
-// PLAY & PAUSE VIDEO 
-function toggleVideoStatus(){
-  return true; 
+// PLAY & PAUSE VIDEO
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
 }
 
-
-
-// UPDATE PLAY / PAUSE ICON 
-function updatePlayIcon(){
-  return true; 
+// UPDATE PLAY / PAUSE ICON
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = `<i class="fa fa-play fa-2x"></i>`;
+  } else {
+    play.innerHTML = `<i class="fa fa-pause fa-2x"></i>`;
+  }
 }
 
+// UPDATE PROGRESS & TIMESTAMP
+function updateProgress() {
+  console.log(video.currentTime); 
+  progress.value = (video.currentTime / video.duration) * 100 ; 
+  let mins = Math.floor(video.currentTime / 60) // get minutes 
+  if(mins < 10){
+    mins = '0' + String(mins)
+  }
+  
+  let secs = Math.floor(video.currentTime % 60) // get seconds  
+  // MODULUS OPERATOR TO GET THE REMAINDER OF 60 
+  if(secs < 10){
+    secs = '0' + String(secs)
+  }
+  timestamp.innerHTML = `${mins}:${secs}` 
 
-
-// UPDATE PROGRESS & TIMESTAMP 
-function updateProgress(){
-  return true;
 }
 
-
-// SET VIDEO TIME TO PROGRESS BAR 
-function setVideoProgress(){
-  return true;
+// SET VIDEO TIME TO PROGRESS BAR
+function setVideoProgress() {
+ video.currentTime = (+progress.value * video.duration / 100);  
 }
 
-
-
-
+// STOP VIDEO
+function stopVideo() {
+  video.currentTime = 0; 
+  video.pause(); 
+}
 
 // EVENT LISTENERS
 video.addEventListener('click', toggleVideoStatus);
