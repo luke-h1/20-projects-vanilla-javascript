e4a95595a88789abecf5c9cfe83757b2c3d5dc0b
luke-h1 <luke.howsam@yahoo.com>
2020-08-27 18:55:06 +0100

    getting correct result from map()

--- section-9-meal-finder-fetch-mealdb-api/app.js
+++ section-9-meal-finder-fetch-mealdb-api/app.js
@@ -3,32 +3,48 @@ const search = document.getElementById('search'),
   random = document.getElementById('random'),
   mealsEl = document.getElementById('meals'),
   resultHeading = document.getElementById('result-heading'),
-  single_mealEl = document.getElementById('meal');
+  single_mealEl = document.getElementById('meal'),
+  errorHeading = document.getElementById('error-heading');
 
 // EVENT LISTENERS
 
+
 function searchMeal(e) {
-  e.preventDefault(); 
-  single_mealEl.innerHTML = ''; 
-  const query = search.value; // get search term
-  // check for empty query 
-  if(query.trim()){
+  e.preventDefault();
+  single_mealEl.innerHTML = '';
+  const query = search.value;
+  if (query.trim()) {
     const API_URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
     fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-      });  
-    
-  }else { 
-    const div = document.getElementById('result-heading') 
-    const errorMsg = `
-    <h1 class="error"> Enter a correct value ❌`; 
-    div.innerHTML = errorMsg; 
-    window.setTimeout(() => { 
-      div.innerHTML = ''; 
-    }, 2000)
+        resultHeading.innerHTML = `<h2>Search results for '${query}' : </h2>`;
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<h2>No search results for '${query}' 🤷‍♂️</h2>`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              (meal) => `
+            <div class="meal"> 
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}" /> 
+              <div class="meal-info" data-mealID="${meal.idMeal}"> 
+              <h3>${meal.strMeal}</h3> 
+              </div> 
+              <div> 
+            `
+            )
+            .join('');
+        }
+      
+      });
+  } else {
+    errorHeading.innerHTML = `<h1 class="error">Enter a correct value ❌</h1>`;
+    window.setTimeout(() => {
+      errorHeading.innerHTML = '';
+    }, 2000);
   }
 }
 
 submit.addEventListener('submit', searchMeal);
+
