42e32525aa69360aabb3a2637c5f393eaaa6c729
luke-h1 <luke.howsam@yahoo.com>
2020-09-01 19:39:50 +0100

    updat

--- section-16-lyrics-search-app-fetch-pagination/app.js
+++ section-16-lyrics-search-app-fetch-pagination/app.js
@@ -13,20 +13,72 @@ const API_URL = `https://api.lyrics.ovh`;
 //     });
 // }
 
-// put into DOM 
-function showData(data){}
-
-
-
-// Fetch from API
-async function searchSongs(term){
-  const res = await fetch(`${API_URL}/suggest/${term}`);
+// get previous and next songs pagination results 
+async function getMoreSongs(url){
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
   const data = await res.json();
-  console.log(data);
   showData(data);
 }
 
 
+
+// Fetch from API
+async function searchSongs(term) {
+  const res = await fetch(`${API_URL}/suggest/${term}`);
+  const data = await res.json();
+  console.log(data);
+  showData(data); 
+} 
+
+// put into DOM
+function showData(data) {
+  // let output = '';
+  // data.data.forEach((song) => {
+  //   output += `
+  //     <li><span><strong>${song.artist.name}</strong>- ${song.title}</span>
+  //     <button class="btn" data-artist=${song.artist.name} daata-songtitle="${song.title}">Get Lyrics:</button>
+  //     </li> 
+  //   `;
+  // });
+  // result.innerHTML = `
+  // <ul class="songs">${output}</ul>
+  // `;
+
+  result.innerHTML = `
+  <ul class="songs">
+  ${data.data.map(song =>  `
+    <li><span><strong>${song.artist.name}</strong>- ${song.title}</span>
+    <button class="btn" data-artist=${song.artist.name} data-songtitle="${song.title}">Get Lyrics:</button>
+    </li>`
+  )
+  .join('')
+  }
+  </ul>
+  `; 
+
+  if(data.prev || data.next){
+    more.innerHTML = `
+    ${data.prev ? `<button class="btn" onClick="getMoreSongs(${data.prev})">Prev</button>` : ''}
+    ${data.next ? `<button class="btn" onClick="getMoreSongs(${data.next})">Next</button>` : ''}
+    `; 
+  }else { 
+    more.innerHTML = '';
+  }
+
+} 
+
+
+
+async function getLyrics(artist, title) {
+  const res = await fetch(`${API_URL}/v1/${artist}/${title}`);
+  const data = await res.json();
+  console.log(data);
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>') // regex for lyrics. replace anything that has a slash r or slash n (return / new line) with a html <br>
+  result.innerHTML =  `
+  <h2><strong>${artist}</strong>- ${title}</h2>
+  <span>${lyrics}</span>`;
+  more.innerHTML = '';
+}
 
 // EVENT LISTENERS
 form.addEventListener('submit', (e) => {
@@ -39,3 +91,16 @@ form.addEventListener('submit', (e) => {
     searchSongs(searchTerm);
   }
 });
+
+
+// get song lyrics button click 
+result.addEventListener('click', e => { 
+  console.log(e.target)
+  const clickedEl = e.target; 
+  if(clickedEl.tagName === 'BUTTON'){ // check tagname for button 
+    console.log('you clicked the button and this listener was 🔥');
+    const artist = clickedEl.getAttribute('data-artist');
+    const title = clickedEl.getAttribute('data-songtitle');
+    getLyrics(artist, title);
+  }
+})
