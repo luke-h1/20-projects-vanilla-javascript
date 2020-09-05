8f1c9b26ad73e337301826c07d36528652f865fe
luke-h1 <luke.howsam@yahoo.com>
2020-09-05 11:47:50 +0100

    update proj

--- section-19-new-year-countdown-dom-date-time/app.js
+++ section-19-new-year-countdown-dom-date-time/app.js
@@ -3,19 +3,30 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countDown = document.getElementById('countdown');
-
-
-
-const currentYear = new Date().getFullYear(); 
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
+const currentYear = new Date().getFullYear();
 const newYearTime = new Date(`Jan 01 ${currentYear + 1}  00:00:00`);
 
+// set bkgrd yr
+year.innerHTML = currentYear + 1;
 
-function updateCountDown(){
+function updateCountDown() {
   const currentTime = new Date();
-  const diff = newYearTime - currentTime; 
-  console.log(diff)
+  const diff = newYearTime - currentTime;
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24); // get number of days
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24; // get number of hours
+  const m = Math.floor(diff / 1000 / 60) % 60; // get number of minutes
+  const s = Math.floor(diff / 1000) % 60; // get number of seconds
+  setTimeout(() => {
+    loading.remove();
+    countDown.style.display = 'flex';
+  }, 1000);
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? '0' + h : h;
+  minutes.innerHTML = m < 10 ? '0' + m : m;
+  seconds.innerHTML = s < 10 ? '0' + s : s;
 }
+setInterval(updateCountDown, 1000);
 
-
-updateCountDown(); 
-
+// show spinner before countdown:
